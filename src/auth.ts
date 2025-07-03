feea4fd85834c2dfd5274391dba782b27718e041
Thapthai Rattananusarak <knight.thapthai@gmail.com>
2025-07-03 16:01:36 +0700

    Refactor auth.ts: remove fallback for API_BASE_URL to streamline environment variable usage.

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
-const baseUrl = process.env.API_BASE_URL || process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseUrl = process.env.API_BASE_URL;
 
 export const authOptions: NextAuthOptions = {
 
